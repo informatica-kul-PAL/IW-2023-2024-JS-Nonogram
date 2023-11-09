f8eb0866a520e3f25ff5291d3d0aee5c65a3949c
JonasssC <jonas.couwberghs@student.kuleuven.be>
2023-11-09 19:25:05 +0100

    Finished game itself, additional features left (restart button, timer and level selection)

--- code/code.js
+++ code/code.js
@@ -43,16 +43,18 @@ const LEVELS = {
          [ 1, 1, 1, 1,-1, 1,-1, 1,-1, 1, 1,-1,-1,-1,-1],
          [-1, 1, 1, 1,-1,-1, 1,-1, 1,-1,-1,-1,-1,-1,-1],
          [-1,-1, 1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]],
-}
+};
 
 let game;
 let timer;
+
+
 
 class Nonogram {
     field;
     solution;
     lives = 3;
-    move_count = 0;
+    click_count = 0;
 
     timeout_id = -1;
 
@@ -77,7 +79,11 @@ class Nonogram {
     draw_all() {
         this.draw_field();
         this.draw_lives();
-        // this.draw_indicators();
+        this.draw_indicators();
+    }
+
+    draw_lives() {
+        document.getElementById('lives').innerHTML = '<div class="live"></div>'.repeat(this.lives);
     }
 
     draw_field() {
@@ -96,12 +102,45 @@ class Nonogram {
         }
     }
 
-    draw_lives() {
-        document.getElementById('lives').innerHTML = '<div class="live"></div>'.repeat(this.lives);
+    draw_indicators() {
+        const left_indicators = this.calculate_indicators(this.solution);
+        const transposed_top_indicators = this.calculate_indicators(this.sol_columns);
+        const padded_left_indicators = this.pad_indicators(left_indicators);
+        const padded_transposed_top_indicators = this.pad_indicators(transposed_top_indicators);
+        const padded_top_indicators = padded_transposed_top_indicators[0].map((col, i) => padded_transposed_top_indicators.map(row => row[i]));
+        document.getElementById('indicator_left').innerHTML = this.indicators_html(padded_left_indicators);
+        document.getElementById('indicator_upper').innerHTML = this.indicators_html(padded_top_indicators);
+    }
+
+    pad_indicators(indicators) {
+        const size = Math.max(...indicators.map(indicator => indicator.length));
+        const padding = Array(size).fill('');
+        return indicators.map(indicator => padding.slice(indicator.length).concat(indicator));
+    }
+
+    calculate_indicators(lines) {
+        return lines.map((line) => this.calculate_line_indicator(line));
+    }
+
+    calculate_line_indicator(line) {
+        const indicator = line.reduce((res, value) => {
+            if (value === 1)
+                res[res.length - 1]++;
+            else if (res[res.length - 1] !== 0)
+                res.push(0);
+            return res;
+        }, [0]).filter((value) => value !== 0);
+        if (indicator.length === 0) return [0];
+        return indicator;
+    }
+
+    indicators_html(indicators) {
+        return `<tbody>${indicators.map(row => `<tr>${row.map(cell => `<td>${cell}</td>`).join('')}</tr>`).join('')}</tbody>`;
     }
 
     on_click(cell, type) {
         if (this.lives === 0) return;
+        this.click_count++;
         const row = cell.parentNode.rowIndex;
         const column = cell.cellIndex;
         
@@ -119,6 +158,7 @@ class Nonogram {
         this.attempt_to_finish_row(row);
         this.attempt_to_finish_column(column);
         this.draw_field();
+        // this.draw_click_count();
 
         if (this.is_won()) {
             this.show_pop_up("YOU'VE WON", 'steelblue', -1);
@@ -153,6 +193,10 @@ class Nonogram {
 
     sol_column(index) {
         return this.solution.map(row => row[index]);
+    }
+
+    get sol_columns() {
+        return [...Array(this.width).keys()].map((index) => this.sol_column(index));
     }
 
     set_row(index, row) {
